09968370841698b30bd3c3b3a8b4bd57ef8a8214
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 23:23:29 +0100

    Added bot instance to the UpdateDispatcher dispatch method paramters

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
@@ -1,12 +1,13 @@
 package com.telegram.xmasstree_bot.server.service
 
+import com.telegram.xmasstree_bot.bot.XMassTreeBot
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.objects.Update
 
 @Service
 class UpdateDispatcher {
-    fun dispatch(update: Update): BotApiMethod<*> {
+    fun dispatch(update: Update, bot: XMassTreeBot): BotApiMethod<*> {
         throw NotImplementedError()
     }
 }
