281d54d6329f8932d7b6d9b3403652195d09e63c
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 23:49:39 +0100

    Autowiring all handlers casses into UpdateDispatcher class constructor

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
@@ -1,12 +1,19 @@
 package com.telegram.xmasstree_bot.server.service
 
 import com.telegram.xmasstree_bot.bot.XMassTreeBot
+import com.telegram.xmasstree_bot.server.handler.CallbackQueryHandler
+import com.telegram.xmasstree_bot.server.handler.CommandHandler
+import com.telegram.xmasstree_bot.server.handler.MessageHandler
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.objects.Update
 
 @Service
-class UpdateDispatcher {
+class UpdateDispatcher(
+    private val messageHandler: MessageHandler,
+    private val commandHandler: CommandHandler,
+    private val callbackQueryHandler: CallbackQueryHandler
+) {
     fun dispatch(update: Update, bot: XMassTreeBot): BotApiMethod<*> {
         throw NotImplementedError()
     }
