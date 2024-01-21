5b045fe11e404cbd47cd3ca96275993c69044521
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 16:41:11 +0100

    Add class for loading Telegram bot configuration properties

---
+++ src/main/kotlin/com/telegram/xmasstree_bot/bot/config/TelegramBotProperties.kt
@@ -0,0 +1,12 @@
+package com.telegram.xmasstree_bot.bot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "telegram")
+data class TelegramBotProperties(
+    private val botName: String? = null,
+    private val botToken: String? = null,
+    private val webHookUrl: String? = null
+)
