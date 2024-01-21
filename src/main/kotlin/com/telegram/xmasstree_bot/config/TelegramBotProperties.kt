ebc9b65dc0b8aeaa0c43603cf8311aafded6984e
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 18:13:29 +0100

    Resolved the problem with injecting properties from application.properties

--- src/main/kotlin/com/telegram/xmasstree_bot/config/TelegramBotProperties.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/config/TelegramBotProperties.kt
@@ -1,21 +1,20 @@
 package com.telegram.xmasstree_bot.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 
-@Configuration
 @ConfigurationProperties(prefix = "telegram")
-class TelegramBotProperties() {
-    private val botName: String = ""
-    private val botToken: String = ""
-    private val webHookUrl: String = ""
+data class TelegramBotProperties @ConstructorBinding constructor(
+    private val botToken: String,
+    private val botName: String,
+    private val webHookUrl: String,
+) {
+    fun getBotToken(): String {
+        return botToken
+    }
 
     fun getBotName(): String {
         return botName
-    }
-
-    fun getBotToken(): String {
-        return botToken
     }
 
     fun getWebHookUrl(): String {
