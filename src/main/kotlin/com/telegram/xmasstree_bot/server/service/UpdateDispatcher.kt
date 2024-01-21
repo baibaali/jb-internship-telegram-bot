124953f822d1525725acf2706a1e2e2b3c846352
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 18:23:07 +0100

    Changed method signature in UpdateDispatcher class

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/UpdateDispatcher.kt
@@ -1,10 +1,12 @@
 package com.telegram.xmasstree_bot.server.service
 
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.objects.Update
 
 @Service
 class UpdateDispatcher {
-    fun dispatch(update: String) {
+    fun dispatch(update: Update): BotApiMethod<*> {
         throw NotImplementedError()
     }
 }
