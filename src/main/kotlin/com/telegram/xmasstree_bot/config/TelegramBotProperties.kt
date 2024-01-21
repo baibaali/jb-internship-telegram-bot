5265cba647dbc72eb838512f572fa8eec024d4a7
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 17:30:16 +0100

    Removed Lombok annotations from TelegramBotProperties class

--- src/main/kotlin/com/telegram/xmasstree_bot/config/TelegramBotProperties.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/config/TelegramBotProperties.kt
@@ -5,8 +5,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "telegram")
-data class TelegramBotProperties(
-    private val botName: String? = null,
-    private val botToken: String? = null,
-    private val webHookUrl: String? = null
-)
+class TelegramBotProperties() {
+    private val botName: String = ""
+    private val botToken: String = ""
+    private val webHookUrl: String = ""
+
+    fun getBotName(): String {
+        return botName
+    }
+
+    fun getBotToken(): String {
+        return botToken
+    }
+
+    fun getWebHookUrl(): String {
+        return webHookUrl
+    }
+}
