547d1ce5c510dcb443bfd2d73865469c25e8597e
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 23:38:14 +0100

    Added CallbackQueryHandler class to handle the callbacks

---
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/handler/CallbackQueryHandler.kt
@@ -0,0 +1,13 @@
+package com.telegram.xmasstree_bot.server.handler
+
+import com.telegram.xmasstree_bot.bot.XMassTreeBot
+import org.jvnet.hk2.annotations.Service
+import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+
+@Service
+class CallbackQueryHandler: AbstractHandler() {
+    override fun handle(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*> {
+        throw NotImplementedError()
+    }
+}
