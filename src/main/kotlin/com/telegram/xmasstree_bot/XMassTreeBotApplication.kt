95bac92233a43c3cdfc7bd12acfbeeacf0c362a1
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-21 17:31:11 +0100

    Added @EnableConfigurationProperties annotation in Application class

--- src/main/kotlin/com/telegram/xmasstree_bot/XMassTreeBotApplication.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/XMassTreeBotApplication.kt
@@ -1,9 +1,12 @@
 package com.telegram.xmasstree_bot
 
+import com.telegram.xmasstree_bot.config.TelegramBotProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(TelegramBotProperties::class)
 class XMassTreeBotApplication
 
 fun main(args: Array<String>) {
