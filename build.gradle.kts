c53146e9f04fa431a2591be499e6c9e59c1a47b2
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 03:33:17 +0100

    Removed unused dependencies. Added dependencies for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,9 +34,11 @@ dependencies {
 
     implementation("org.locationtech.jts:jts-core:1.19.0")
 
-    compileOnly("org.projectlombok:lombok:1.18.30")
-
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("ch.qos.logback:logback-core:1.4.12")
+    implementation("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
