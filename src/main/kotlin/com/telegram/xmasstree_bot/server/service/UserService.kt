b665cbc24f1e4165b7cbbffcedf6dd244efbede1
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 17:54:41 +0100

    Changed services to inherit from AbstractEntityService class

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/UserService.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/UserService.kt
@@ -2,13 +2,10 @@ package com.telegram.xmasstree_bot.server.service
 
 import com.telegram.xmasstree_bot.server.entity.User
 import com.telegram.xmasstree_bot.server.repository.UserRepository
+import com.telegram.xmasstree_bot.server.service.common.AbstractEntityService
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Service
 
 @Service
-class UserService(private val userRepository: UserRepository) {
-
-    fun save(user: User) = userRepository.save(user)
-
-    fun findById(id: Long) = userRepository.findById(id)
-
-}
+class UserService(userRepository: UserRepository):
+    AbstractEntityService<JpaRepository<User, Long>, User, Long>(userRepository)
