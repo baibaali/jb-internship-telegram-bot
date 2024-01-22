c8c3fbb1726f3d83e7f19904865344b25013b3eb
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 03:59:46 +0100

    Cleaned dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,7 @@ dependencies {
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-    implementation("ch.qos.logback:logback-classic:1.4.12")
-    implementation("ch.qos.logback:logback-core:1.4.12")
-    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("org.slf4j:slf4j-api")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
