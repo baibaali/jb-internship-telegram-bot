10ab636206f641c3e6e962aa1ef4cf1155fd4032
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 04:03:58 +0100

    Changed method signature in MessageFactory class

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/factory/MessageFactory.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/factory/MessageFactory.kt
@@ -1,13 +1,13 @@
 package com.telegram.xmasstree_bot.server.service.factory
 
+import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage.SendMessageBuilder
-import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard
 
+@Component
 class MessageFactory: AbstractFactory() {
 
-    fun createMessage(chatId: Long, replyMarkup: ReplyKeyboard, text: String): SendMessage {
+    fun createMessage(chatId: Long, text: String, replyMarkup: ReplyKeyboard?): SendMessage {
         return SendMessage.builder()
             .chatId(chatId.toString())
             .replyMarkup(replyMarkup)
