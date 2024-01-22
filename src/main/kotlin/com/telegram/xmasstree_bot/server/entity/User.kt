c661e35e8d6f716d04e71f9e1a48114ad2ee7101
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 16:18:39 +0100

    Added User entity

---
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/entity/User.kt
@@ -0,0 +1,21 @@
+package com.telegram.xmasstree_bot.server.entity
+
+import com.telegram.xmasstree_bot.server.entity.enums.UserState
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    val id: Long = 0L,
+
+    @Column(name = "username")
+    var username: String = "",
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    var state: UserState = UserState.MENU,
+
+    @Column(name = "banned", nullable = false)
+    var banned: Boolean = false
+)
