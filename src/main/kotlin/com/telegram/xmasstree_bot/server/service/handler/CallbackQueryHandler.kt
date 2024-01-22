baffc68fec08277df680ae471a8b70f60d27567d
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-22 04:08:37 +0100

    Implementation of CallbackQueryHandler

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/handler/CallbackQueryHandler.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/handler/CallbackQueryHandler.kt
@@ -1,13 +1,18 @@
 package com.telegram.xmasstree_bot.server.service.handler
 
 import com.telegram.xmasstree_bot.bot.XMassTreeBot
+import com.telegram.xmasstree_bot.server.service.UserInteractionProcessor
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery
 
 @Service
-class CallbackQueryHandler: AbstractHandler() {
+class CallbackQueryHandler(
+    private val userInteractionProcessor: UserInteractionProcessor
+): AbstractHandler() {
     override fun handle(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*> {
-        throw NotImplementedError()
+        val callbackQuery = botApiObject as CallbackQuery
+        return userInteractionProcessor.processCallbackQuery(callbackQuery, bot)
     }
 }
