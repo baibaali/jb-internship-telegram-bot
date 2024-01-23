799925b8cff8fd5b7fda3477c449a29377b35567
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 12:50:10 +0100

    Added documenatation for AbstractEntityService class

--- src/main/kotlin/com/telegram/xmasstree_bot/server/repository/UserRepository.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/repository/UserRepository.kt
@@ -4,6 +4,9 @@ import com.telegram.xmasstree_bot.server.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+/**
+ * Repository for User entity
+ */
 @Repository
 interface UserRepository: JpaRepository<User, Long> {
 }
--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/common/AbstractEntityService.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/common/AbstractEntityService.kt
@@ -6,20 +6,47 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 
+/**
+ * AbstractService class for entities' services.
+ * @param R - repository class.
+ * @param E - entity class.
+ * @param ID - entity's id class.
+ * @property repository - repository instance.
+ */
 abstract class AbstractEntityService<R : JpaRepository<E, ID>, E : Any, ID : Any>(private var repository: R) {
 
+    /**
+     * Find all entities.
+     * @return list of entities.
+     */
     fun findAll(): List<E> {
         return repository.findAll()
     }
 
+    /**
+     * Find all entities with pagination.
+     * @param page - page number.
+     * @param size - page size.
+     * @return page of entities.
+     */
     fun findAll(page: Int, size: Int): Page<E> {
         return repository.findAll(PageRequest.of(page, size))
     }
 
+    /**
+     * Find entity by id.
+     * @param id - entity's id.
+     * @return optional of entity.
+     */
     fun findById(id: ID): Optional<E> {
         return repository.findById(id)
     }
 
+    /**
+     * Save entity to database.
+     * @param entity - entity to save.
+     * @return saved entity.
+     */
     fun save(entity: E): E {
         return repository.save(entity)
     }
