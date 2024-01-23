888ee5e8762a8d65b0cfd6a51b75df58de728f80
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 04:49:16 +0100

    Implemented TextMessageStrategy

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/TextMessageStrategy.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/TextMessageStrategy.kt
@@ -1,13 +1,27 @@
 package com.telegram.xmasstree_bot.server.service.strategy
 
 import com.telegram.xmasstree_bot.bot.XMassTreeBot
+import com.telegram.xmasstree_bot.server.entity.enums.UserState
+import com.telegram.xmasstree_bot.server.service.UserService
+import com.telegram.xmasstree_bot.server.service.factory.BotPredefinedMessageFactory
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.objects.Message
 
 @Service
-class TextMessageStrategy: MessageStrategy {
+class TextMessageStrategy(
+    private val userService: UserService,
+    private val botPredefinedMessageFactory: BotPredefinedMessageFactory
+): MessageStrategy {
     override fun execute(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*>? {
-        throw UnsupportedOperationException("Not yet implemented")
+        val message = botApiObject as Message
+        return processText(message, bot)
+    }
+
+    fun processText(message: Message, bot: XMassTreeBot): BotApiMethod<*>? {
+        val user = userService.getOrCreateUser(message.from)
+        userService.updateUserState(user, UserState.MENU)
+        return botPredefinedMessageFactory.returnToMenuSend(message.chatId)
     }
 }
