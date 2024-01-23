d5d5aff938a3ea66cd7c80ccb0ef98dc17e3c009
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 12:42:17 +0100

    Added documentation for user-related enums

--- src/main/kotlin/com/telegram/xmasstree_bot/server/entity/enums/City.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/entity/enums/City.kt
@@ -1,5 +1,9 @@
 package com.telegram.xmasstree_bot.server.entity.enums
 
+/**
+ * City enum.
+ * Represents cities where bot can be used.
+ */
 enum class City {
     PRAGUE,
 }
--- src/main/kotlin/com/telegram/xmasstree_bot/server/entity/enums/UserState.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/entity/enums/UserState.kt
@@ -1,5 +1,9 @@
 package com.telegram.xmasstree_bot.server.entity.enums
 
+/**
+ * User state enum.
+ * Represents user states in bot.
+ */
 enum class UserState {
     MENU,
     LOCATION,
