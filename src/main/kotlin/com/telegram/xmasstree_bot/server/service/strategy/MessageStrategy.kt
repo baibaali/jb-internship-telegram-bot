14d038b3cef82d0b1ec6d63adabc3476ddae24af
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 04:28:25 +0100

    Changed the return type of execute method in MessageStrategy interface

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/MessageStrategy.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/MessageStrategy.kt
@@ -2,7 +2,8 @@ package com.telegram.xmasstree_bot.server.service.strategy
 
 import com.telegram.xmasstree_bot.bot.XMassTreeBot
 import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 
 interface MessageStrategy {
-    fun execute(botApiObject: BotApiObject, bot: XMassTreeBot)
+    fun execute(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*>?
 }
--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/PhotoMessageStrategy.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/strategy/PhotoMessageStrategy.kt
@@ -3,10 +3,11 @@ package com.telegram.xmasstree_bot.server.service.strategy
 import com.telegram.xmasstree_bot.bot.XMassTreeBot
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 
 @Service
 class PhotoMessageStrategy: MessageStrategy {
-    override fun execute(botApiObject: BotApiObject, bot: XMassTreeBot) {
+    override fun execute(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*>? {
         throw UnsupportedOperationException("Not yet implemented")
     }
 }
