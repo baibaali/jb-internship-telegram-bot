8788769a2f8a2559a73bb6d72ead1ce2346f1a47
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 12:46:52 +0100

    Added documentation for custom exception classes

--- src/main/kotlin/com/telegram/xmasstree_bot/server/exception/InvalidArgumentException.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/exception/InvalidArgumentException.kt
@@ -1,4 +1,10 @@
 package com.telegram.xmasstree_bot.server.exception
 
+/**
+ * Exception thrown when an invalid argument is passed to a method.
+ * @param message The message of the exception.
+ * @param cause The cause of the exception.
+ * @see RuntimeException
+ */
 class InvalidArgumentException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
 
--- src/main/kotlin/com/telegram/xmasstree_bot/server/exception/NotInitializedException.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/exception/NotInitializedException.kt
@@ -1,3 +1,9 @@
 package com.telegram.xmasstree_bot.server.exception
 
+/**
+ * Exception thrown when some field is not initialized.
+ * @param message The message of the exception.
+ * @param cause The cause of the exception.
+ * @see RuntimeException
+ */
 class NotInitializedException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
