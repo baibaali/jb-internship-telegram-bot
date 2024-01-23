5e84ea649ad2a51e937b2ffd6e5ec76d325ad588
Alisher Baibatchinov <baibatchinova@gmail.com>
2024-01-23 05:18:55 +0100

    Added proceesMessage method to AbstractInteractionProcessor class

--- src/main/kotlin/com/telegram/xmasstree_bot/server/service/interaction/AbstractInteractionProcessor.kt
+++ src/main/kotlin/com/telegram/xmasstree_bot/server/service/interaction/AbstractInteractionProcessor.kt
@@ -1,4 +1,9 @@
 package com.telegram.xmasstree_bot.server.service.interaction
 
+import com.telegram.xmasstree_bot.bot.XMassTreeBot
+import org.telegram.telegrambots.meta.api.interfaces.BotApiObject
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+
 abstract class AbstractInteractionProcessor {
+    abstract fun processMessage(botApiObject: BotApiObject, bot: XMassTreeBot): BotApiMethod<*>?
 }
